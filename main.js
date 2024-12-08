be2f72548a9cea733bce149e3d97ad23917e4962
Kaleb-Castilleja <kalebcastle33@gmail.com>
2024-12-08 11:27:57 -0600

    Commit 4 (Home page)

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 // JavaScript Document
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('nav');
+const navIcon = document.querySelectorAll('.navIcon');
+const hamburger = document.querySelector('#hamburger');
 
-    menuToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('open');
-    });
+
+navToggle.addEventListener("click", () => {
+	nav.classList.toggle('open');
+	navIcon.forEach(icon => {
+		icon.classList.toggle('hidden');
+	});
+});
+	
+
+window.addEventListener("resize", () => {
+	if(document.body.clientWidth > 700) {
+		nav.classList.remove('open');
+		navIcon.forEach(icon => {
+			icon.classList.add('hidden')
+		});
+		hamburger.classList.remove('hidden')
+	}
 });
